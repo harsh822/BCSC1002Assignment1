999d304434ef087ab72826f0239c36c2a6e0c169
harsh822 <harsh.gautam_cs19@gla.ac.in>
2020-08-25 17:37:43 +0530

    add tostring() method in Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -31,6 +31,11 @@ public class Library {
     }
 
     @Override
+    public String toString() {
+        return "availableBooks=" + Arrays.toString(availableBooks) + ".";
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;

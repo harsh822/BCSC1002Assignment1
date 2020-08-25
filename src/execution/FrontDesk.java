c36735c7ee9cefe3f9780117327c93821d532254
harsh822 <harsh.gautam_cs19@gla.ac.in>
2020-08-25 18:01:39 +0530

    add do while loop in FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -8,8 +8,34 @@ package execution;
 
 import definitions.Student;
 
+import java.util.Scanner;
+
 public class FrontDesk {
+    private static final int ISSUE_BOOK = 1;
+    private static final int RETURN_BOOK = 2;
+    private static final int ALL_ISSUED_BOOKS = 3;
+    private static final int Exit = 4;
+
     public static void main(String[] args) {
-        Student detailsOfInteractingStudent;
+        Scanner scanner = new Scanner(System.in);
+        int studentInput;
+        Student detailsOfInteractingStudent = new Student();
+        do {
+            System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-");
+            System.out.println("How may I help you today?");
+            System.out.println("1. Issue a new book for me.");
+            System.out.println("2. Return a previously issues book for me.");
+            System.out.println("3. Show me all my issues books.");
+            System.out.println("4. Exit.");
+            System.out.println("Enter your choice (1..4): ");
+            studentInput = scanner.nextInt();
+            switch (studentInput) {
+                case ISSUE_BOOK:
+                    System.out.println("Enter the name of the book you want to issue: ");
+                    scanner.nextLine();
+                    break;
+
+            }
+        } while (studentInput != Exit);
     }
 }
